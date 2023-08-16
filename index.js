8071e5d013602b064431cf0854e4fa15b025323d
unknown <Sophia>
2023-08-16 16:25:57 +0300

    Проверка паролей и база данных

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import { router } from './src/routers/index.js'
 
 const app = express()
@@ -8,7 +9,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/', router)
 
 app.set('view engine', 'ejs')
+app.set('views', './src/views')
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+    await mongoose.connect(
+        'mongodb+srv://SofikoG:V3uk9JFc27whnQdq@cluster0.v0cr5qr.mongodb.net/'
+    )
     console.log('Сервер запущен по адресу: http://localhost:3000')
 })
+
+//mongodb+srv://SofikoG:V3uk9JFc27whnQdq@cluster0.v0cr5qr.mongodb.net/
--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,13 +1,26 @@
+import User from '../models/User.js'
+
 export function getRegister(req, res) {
     return res.render('register', {})
 }
 
-export function postRegister(req, res) {
-    const { password, conPassword } = req.body
-
+export async function postRegister(req, res) {
+    const { login, password, conPassword } = req.body
     if (password == conPassword) {
+        const userData = { login, password }
+        const user = await User.create(userData)
+        console.log(user)
         return res.send('Пользователь зарегистрирован')
     } else {
         return res.send('Ошибка при регистрации (пароли не совпадают)')
     }
+    // const userData = { login, password }
+    // const user = await User.create(userData)
+    // console.log(user)
+    // return res.send(user)
+    // if (password == conPassword) {
+    //     return res.send('Пользователь зарегистрирован')
+    // } else {
+    //     return res.send('Ошибка при регистрации (пароли не совпадают)')
+    // }
 }
